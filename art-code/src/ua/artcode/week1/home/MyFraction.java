181d4b908343761ceb54d6d31f1354ca47a9ef47
skazako <semyonkazakov@gmail.com>
2016-01-22 02:55:18 +0200

    MyNumber with tests - completed

--- art-code/src/ua/artcode/week1/home/MyFraction.java
+++ art-code/src/ua/artcode/week1/home/MyFraction.java
@@ -5,4 +5,105 @@ package ua.artcode.week1.home;
  */
 public class MyFraction {
 
+    private int numerator;
+    private int denominator;
+
+    public MyFraction(int numerator, int denominator){
+        this.numerator = numerator;
+        this.denominator = denominator;
+    }
+
+    public String toString(){
+        return this.numerator + "/" + this.denominator;
+    }
+
+    public MyFraction reduce(MyFraction other){
+        int max = 0;
+        if (other.numerator > other.denominator)
+            max = Math.abs(other.denominator);
+        else
+            max = Math.abs(other.numerator);
+        for (int i = max; i >= 2; i--){
+            if ((other.numerator % i == 0) && (other.denominator % i == 0)){
+                other.numerator /= i;
+                other.denominator /= i;
+            }
+        }
+        if (other.denominator < 0){
+            other.numerator = -1 * (other.numerator);
+            other.denominator = Math.abs(other.denominator);
+        }
+        return other;
+    }
+
+
+
+    public MyFraction add(MyFraction other){
+        MyFraction res = new MyFraction(0,0);
+        res.numerator = (this.numerator * other.denominator + this.denominator * other.numerator);
+        res.denominator = (this.denominator * other.denominator);
+        reduce(res);
+        return res;
+    }
+
+    public MyFraction extract(MyFraction other){
+        MyFraction res = new MyFraction(0,0);
+        res.numerator = (this.numerator * other.denominator - this.denominator * other.numerator);
+        res.denominator = (this.denominator * other.denominator);
+        reduce(res);
+        return res;
+    }
+
+    public MyFraction multiply(MyFraction other){
+        MyFraction res = new MyFraction(0,0);
+        res.numerator = (this.numerator * other.numerator);
+        res.denominator = (this.denominator * other.denominator);
+        reduce(res);
+        return res;
+    }
+
+    public MyFraction divide(MyFraction other){
+        MyFraction res = new MyFraction(0,0);
+        res.numerator = (this.numerator * other.denominator);
+        res.denominator = (this.denominator * other.numerator);
+        reduce(res);
+        return res;
+    }
+
+    public String compare(MyFraction other){
+        if ((( this.numerator * 100 ) / this.denominator ) > (( other.numerator * 100 ) / other.denominator ))
+            return this.numerator + "/" + this.denominator + " is bigger than " + other.numerator + "/" + other.denominator;
+        if ((( this.numerator * 100 ) / this.denominator ) > (( other.numerator  * 100 ) / other.denominator ))
+            return other.numerator + "/" + other.denominator + " is bigger than " + this.numerator + "/" + this.denominator;
+        return other.numerator + "/" + other.denominator + " equals " + this.numerator + "/" + this.denominator;
+    }
+
+
+/*
+    public MyNumber(double value) {
+        this.value = value;
+    }
+
+
+    public MyNumber add(MyNumber other){
+        return new MyNumber(this.value + other.value);
+    }
+
+    public MyNumber extract(MyNumber other){
+        return new MyNumber(this.value - other.value);
+    }
+
+    public MyNumber multiply(MyNumber other){
+        return new MyNumber(this.value * other.value);
+    }
+
+    public MyNumber divide(MyNumber other){
+        return new MyNumber(this.value / other.value);
+    }
+
+    public MyNumber power(MyNumber other){
+        return new MyNumber(Math.pow(this.value,other.value));
+    }
+ */
+
 }
--- art-code/src/ua/artcode/week1/home/TestMyFraction.java
+++ art-code/src/ua/artcode/week1/home/TestMyFraction.java
@@ -5,6 +5,46 @@ package ua.artcode.week1.home;
  */
 public class TestMyFraction {
     public static void main(String[] args) {
+        MyFraction fract1 = new MyFraction(2,4);
+        MyFraction fract2 = new MyFraction(1,3);
+
+        MyFraction fract3 = fract1.add(fract2);
+        MyFraction fract4 = fract1.extract(fract2);
+        MyFraction fract5 = fract1.multiply(fract2);
+        MyFraction fract6 = fract1.divide(fract2);
+
+
+
+        //add
+        if (fract3.toString().equals("5/6"))
+            System.out.println("TEST-OK: " + "add: " + fract3.toString());
+        if (!fract3.toString().equals("5/6"))
+            System.out.println("ERROR: " + "add is " + fract3.toString() + " but should be: 5/6");
+
+               //extract test
+        if (fract4.toString().equals("1/6"))
+            System.out.println("TEST-OK: " + "extract: " + fract4.toString());
+        if (!fract4.toString().equals("1/6"))
+            System.out.println("ERROR: " + "extract is " + fract4.toString() + " but should be: 1/6");
+
+        //multiply test
+        if (fract5.toString().equals("1/6"))
+            System.out.println("TEST-OK: " + "multiply: " + fract5.toString());
+        if (!fract5.toString().equals("1/6"))
+            System.out.println("ERROR: " + "multiply is " + fract5.toString() + " but should be: 1/6");
+
+        //divide test
+        if (fract6.toString().equals("3/2"))
+            System.out.println("TEST-OK: " + "divide: " + fract6.toString());
+        if (!fract6.toString().equals("3/2"))
+            System.out.println("ERROR: " + "divide is " + fract6.toString() + " but should be: 3/2");
+
+        //compare test
+        if (fract1.compare(fract2).equals("2/4 is bigger than 1/3"))
+            System.out.println("TEST-OK: " + "compare: " + fract1.compare(fract2));
+        if (!fract1.compare(fract2).equals("2/4 is bigger than 1/3"))
+            System.out.println("ERROR: " + "compare is " + fract1.compare(fract2) + " but should be: 2/4 is bigger than 1/3");
+
 
     }
 }
